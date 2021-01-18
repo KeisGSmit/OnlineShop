920e97829d9c0797010cb62a1c924263c289f1b5
KeisGSmit <kgsmit98@gmail.com>
2021-01-18 12:00:15 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IAvHVECTsEQEyp4uaa1Ygz9AQGg1As3mODsvFGRNI9RLcpPTPLv58rxFfRK1j831KYRBlETVze7VMlFI7qo2eIY00An40UxNo',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
